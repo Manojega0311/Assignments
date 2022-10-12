4fa4d5b92701c7cdd9bca7353de27005374edcb3
Manovadhana M <113025165+Manojega0311@users.noreply.github.com>
2022-10-12 10:15:29 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,23 @@
+//wrp to demo program using super()keyword
+//Manovadhana M CG BATCH 2576
+class Animal
+{
+	public static void main(String[]args)
+	{
+		
+	Void Animal();
+	
+	{
+		System.out.println("Animal is created");
+		
+	}
+}
+class Dog extends Animal
+{
+	Void Dog();
+		{
+		super();
+		System.out.println("Dod is created");
+	}
+}
+}
