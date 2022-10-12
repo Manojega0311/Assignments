3873b5ff6d11b3dd1e2af6936b0f0ef2f4c8f024
Manovadhana M <113025165+Manojega0311@users.noreply.github.com>
2022-10-12 10:16:17 +0530

    Add files via upload

---
+++ Test5.java
@@ -0,0 +1,43 @@
+//wrp to demo Abstract class Interface implementation
+//Manovadhana M CG BATCH 2576
+interface A
+{
+	void a();
+	void b();
+	void c();
+	void d();
+} 
+abstract class B implements A
+{
+ public void c()
+ {
+	 System.out.println("i am c");
+ } 
+}
+class M extends B
+{
+	public void a()
+	{
+	System.out.println("i am a");	
+	}
+
+public void b()
+{
+	System.out.println("i am b");
+}
+public void d()
+{
+	System.out.println("i am d");
+}
+}
+class Test5
+{
+	public static void main(String[]args)
+	{
+		A a = new M();
+		a.a();
+		a.b();
+		a.c();
+		a.d();
+	}
+}
